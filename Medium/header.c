d604fc0ef8513e6a4fa4a8c6048b7da2b861735f
Manas <122614493+manas-gupta-3131@users.noreply.github.com>
2024-12-07 14:37:00 +0530

    Create header.c

---
+++ Medium/header.c
@@ -0,0 +1,14 @@
+//shell.h
+#ifndef SHELL_H
+#define SHELL_H
+
+/*---LIBRARIES---*/
+#include <stdio.h>
+#include <unistd.h>
+
+/*---PROTOTYPES---*/
+/* main.c */
+void shell_interactive(void);
+void shell_no_interactive(void);
+
+#endif
